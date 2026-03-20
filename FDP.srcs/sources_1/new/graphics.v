140f57ee6a430ccd57d48aa9879c88ca65f5d822
Khairul Haziq <khaihaziq2001@gmail.com>
2026-03-21 00:59:42 +0800

    Add movement to cloud with wrap-around behaviour

--- FDP.srcs/sources_1/new/graphics.v
+++ FDP.srcs/sources_1/new/graphics.v
@@ -36,6 +36,8 @@ module graphics(
     wire [6:0] y;
     wire [6:0] x;
     
+    reg [20:0] timer_1 = 0;
+    
     Oled_Display disp (
         .clk(clk),
         .reset(0),
@@ -56,19 +58,34 @@ module graphics(
     assign y = pixel_index / 96;
     assign x = pixel_index % 96;
     
+    parameter MAX_X = 95;
+    parameter MAX_Y = 63;
+    
     // Cloud dimensions
     parameter CLOUD_W = 25;
     parameter CLOUD_H = 10;
-    parameter CLOUD_X = 40;
-    parameter CLOUD_Y = 5;
+    reg [6:0] CLOUD_X = 0;
+    reg [6:0] CLOUD_Y = 5;
     
-    // Draw background
+    // Timer for cloud movement
+    always @(posedge clk) begin
+        timer_1 <= (timer_1 == 1_562_500) ? 0 : timer_1 + 1;
+        
+        if (timer_1 == 0) CLOUD_X <= (CLOUD_X + 1) % 96;
+    end
+    
+    // Draw layers
     always @ (posedge clk) begin
         // Background layer: sky, grass and clouds
         if (y >= 50) pixel_data = 16'b00000_111111_00000;
         else pixel_data = 16'b00000_000000_11111;
         
-        if (x >= CLOUD_X && x <= CLOUD_X + CLOUD_W && y >= CLOUD_Y && y <= CLOUD_Y + CLOUD_H) pixel_data = 16'b11111_111111_11111;
+        // Cloud
+        if (y >= CLOUD_Y && y <= CLOUD_Y + CLOUD_H) begin
+            // Compute cloud pixel relative position with modulo
+            if (((x + 96 - CLOUD_X) % 96) < CLOUD_W)
+                pixel_data = 16'b11111_111111_11111;
+        end
     end
     
 endmodule
